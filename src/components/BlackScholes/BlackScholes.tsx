79f1a5dda30a98f3f0ca0d72bf985b7f68c7975e
sabidobrin <sabi.dobrin@gmail.com>
2021-08-31 22:31:12 +0200

    black-scholes-fields

--- src/components/BlackScholes/BlackScholes.tsx
+++ src/components/BlackScholes/BlackScholes.tsx
@@ -3,28 +3,35 @@ import './black-scholes.css';
 import { Container, Row, Col, Form } from 'react-bootstrap';
 import { Submit } from '../../context';
 
-export interface parameters {
-    callOptionPrice: number;
+/*export interface Iparameters {
     currentUnderlyingPrice: number;
     strikePrice: number;
     riskFreeInterestRate: number;
     timeToMaturity: number;
     normalDistribution: number;
-}
+}*/
 
 export default function BlackScholes (props: any) {
 
-    const [parameters, setParameters] = useState({
-        callOptionPrice: undefined,
+    /*const [parameters, setParameters] = useState({
         currentUnderlyingPrice: undefined,
         strikePrice: undefined,
         riskFreeInterestRate: 0,
         timeToMaturity: undefined,
         normalDistribution: undefined
-    });
+    });*/
 
-    const handleSubmit = () => {
-        console.log("clicked");
+    const [currentUnderlyingPrice, setCurrentUnderlyingPrice] = useState(0);
+    const [strikePrice, setStrikePrice] = useState(0);
+    const [timeToMaturity, setTimeToMaturity] = useState(0);
+
+    const [riskFreeInterestRate, setRiskFreeInterestRate] = useState(0);
+    const [normalDistribution, setNormalDistribution] = useState(0);
+    const [callOptionPrice, setCallOptionPrice] = useState(0);
+
+    const handleSubmit = (event: any) => {
+        let price:number = 0;
+        setCallOptionPrice(price);
     }
 
     return (
@@ -35,35 +42,27 @@ export default function BlackScholes (props: any) {
             <Row>
                 <Col xs={12} sm={12} md={6} lg={6}>
                 <Form.Group className="mb-3" controlId="black-scholes">
-                    <Form.Label>Call option price</Form.Label>
-                    <Form.Control type="text" placeholder="Call option price" value={parameters.callOptionPrice} />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="black-scholes">
                     <Form.Label>Current underlying price</Form.Label>
-                    <Form.Control type="text" placeholder="Current underlying price" value={parameters.currentUnderlyingPrice} />
+                    <Form.Control type="text" placeholder="Current underlying price" value={currentUnderlyingPrice} />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="black-scholes">
                     <Form.Label>Strike price</Form.Label>
-                    <Form.Control type="text" placeholder="Strike price" value={parameters.strikePrice} />
+                    <Form.Control type="text" placeholder="Strike price" value={strikePrice} />
                 </Form.Group>
                 </Col>
                 <Col xs={12} sm={12} md={6} lg={6}>
                 <Form.Group className="mb-3" controlId="black-scholes">
-                    <Form.Label>Risk-free interest rate</Form.Label>
-                    <Form.Control plaintext readOnly defaultValue="0" value={parameters.riskFreeInterestRate} />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="black-scholes">
                     <Form.Label>Time to maturity</Form.Label>
-                    <Form.Control type="text" placeholder="Time to maturity" value={parameters.timeToMaturity} />
+                    <Form.Control type="text" placeholder="Time to maturity" value={timeToMaturity} />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="black-scholes">
-                    <Form.Label>Normal distribution</Form.Label>
-                    <Form.Control type="text" placeholder="Normal distribution" value={parameters.normalDistribution} />
+                    <Form.Label>Risk-free interest rate</Form.Label>
+                    <Form.Control plaintext readOnly defaultValue={riskFreeInterestRate} value={riskFreeInterestRate} />
                 </Form.Group>
                 </Col>
             </Row>
-            
             <Row><Submit handleSubmit={handleSubmit} /></Row>
+            <Row><b>Call option price: {callOptionPrice}</b></Row>
             </Form>
             </Col>
             
